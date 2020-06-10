d57de5d9a975fd4c27aeae54166b0ce97d186f33
kjy12134 <kjy12134@naver.com>
2020-06-11 00:08:25 +0900

    0611

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
-const express = require('express'); // import
+//const express = require('express'); // import
+import express from "express";
+
 const app = express(); // get
 const PORT = 4000;
 
-function handleListening() {
-    console.log('Listening on http://localhost:${PORT}');
-};
-
-
-function handlehome(req, res) {
-    console.log(req);
-    res.send('HELLO FROM HOME');
-}
-
-function handleProfile(req, res) {
-    res.send('HELLO FROM PROFILE');
-}
+// es6 ver js !!
+const handleListening = () => console.log('Listening on http://localhost:${PORT}');
+const handlehome = (req, res)  => res.send('HELLO FROM HOME');
+const handleProfile = (req, res) => res.send('HELLO FROM PROFILE');
+const handleTest = (req, res) => res.send('메롱 김지영 우하하');
 
 app.get("/", handlehome);
 app.get("/profile", handleProfile);
-
-
+app.get("/test", handleTest);
 app.listen(PORT, handleListening);
